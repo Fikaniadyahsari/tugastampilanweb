1f7a6fe8816d29adb7e451e5517669e78ed413df
unknown <fikafrianka@example.com>
2022-08-30 17:15:10 +0700

    tugastampilanweb

---
+++ CodeIgniter-3.1.13/application/views/umum/tugasweb.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pencarian</title>
+</head>
+<body bgcolor="#FFF8DC">
+	<h1>Tampilan Pencarian</h1>
+	<h2>Kelompok 7 | Destriana, Dewinta, Fika, Ghina</h2>
+
+	<form align="center">
+		<label>Cari :</label>
+		<input type="text" placeholder="cari apa nich">
+		<input type="submit" name="cari">
+	</form>
+	<p>Destriana Surya Ningtyas, nama panggilannya adalah ndes. Bercita-cita untuk kuliah di Universitas ternama dan mengambil jurusan Sastra Indonesia. Saya adalah fangirl EXO, bias saya adalah D.O. Kyungsoo. Terima Kasih... </p>
+	<p>Dewinta Ramadhani, biasanya dipanggil dewinn. Hobi saya sama seperti anak remaja pada umumnya, yaitu kalau gak nonton drakor ya play spotify xixixi. Saya memiliki cita-cita banyak. cukup sekian wassalam...</p>
+	<p>Fika Nia Dyahsari adalah namanya. Dia sering dipanggil fika. Ciri ciri seorang fika adalah tinggi dan mempunyai hidung yang lancip. etss sekarang saya yang ambil alihh. hobi saya adalah bersantai and tenang wkwkwk. sekian dari saya wassalamualaikumm sehat sehat teruss !!!!</p>
+	<p>Ghina Satiroh, nama panggilan saya adalah ghina. hobi saya adalah main basket. bias saya adalah chanyeol dan saya suka messi. sekian dari saya wassalam.... :)</p>
+</body>
+</html>
